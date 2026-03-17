310507d2626d92f16809455affa3cf003e8ff6d8
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-03-17 03:44:39 +0100

    feat: aggiungo funzione index in movieController

---
+++ controllers/movieController.js
@@ -0,0 +1,24 @@
+// importo la connessione al database
+const connection = require("../database/conn");
+
+// creo la funzione per ottenere la lista dei film
+function index(req, res) {
+  // faccio la query per ottenere tutti i film
+  const sql = "SELECT * FROM movies";
+
+  // callback - gestisco il risultato  della query
+  connection.query(sql, (err, results) => {
+    // se c'è un errore restituiscimi 500
+    if (err) {
+      return res.status(500).json({
+        message: "Errore nel recupero dei film.",
+        success: false,
+      });
+    }
+    res.json({
+      message: "Lista film recuperata con successo Gabriela!",
+      success: true,
+      results: results,
+    });
+  });
+}
